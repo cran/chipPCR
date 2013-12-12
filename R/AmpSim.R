e9a9c8aa7e322a027594f01c65de64925262ac9d
Stefan Roediger <stefan.roediger@hs-lausitz.de>
2013-12-12 00:00:00 +0000

    version 0.0.5

---
+++ R/AmpSim.R
@@ -0,0 +1,37 @@
+AmpSim <- function(cyc = c(1:35), b.eff = -25, bl = 0.05, ampl = 1, 
+		   Cq = 20, noise = FALSE, nnl = 0.025, 
+		   nnl.method = "constant")
+{
+  tmp_warn <- getOption("warn")
+  options(warn = -1)
+  # Test if x and y exist and have identical lengths.
+  if (is.null(cyc)) 
+    stop("Enter cycle values")
+  if (nnl < 0 || nnl > 0.1) 
+    stop("nnl must be within 0 and 0.1.")
+   
+  fluo <- bl + (ampl - bl)/(1 + exp(b.eff * (log(cyc) - log(Cq))))
+  if (noise) {
+  mean.noise <- mean(fluo) * nnl
+  sd.noise <- sd(fluo) * nnl
+  if (nnl.method == "increase") {
+		NOISE <- sort(rnorm(length(fluo), 
+				mean = mean.noise, 
+				sd = sd.noise))
+		}
+  if (nnl.method == "decrease") {
+		NOISE <- sort(rnorm(length(fluo), 
+		mean = mean.noise, 
+		sd = sd.noise), decreasing = TRUE)
+		}
+  if (nnl.method == "constant") {
+		NOISE <- rnorm(length(fluo), 
+				mean = mean.noise, 
+				sd = sd.noise)
+		}
+  fluo <- fluo + NOISE
+  res <- cbind(cyc, fluo)
+  } else (res <- cbind(cyc, fluo))
+  options(warn = tmp_warn)
+  res
+}
